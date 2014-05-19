a0ca305c580d067cbde6611e25400c59c7877516
Nada Mostafa <nada.fadali@gmail.com>
2014-05-19 08:06:04 +0200

    user input data case, user input starting address case handeled

--- src/components/Data_Memory.java
+++ src/components/Data_Memory.java
@@ -1,56 +1,71 @@
 package components;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Data_Memory {
 	private int address;
 	private int write_Data;
 	private int read_Data;
-	private ArrayList<Integer> memory;
+	// private ArrayList<Integer> memory;
+	private HashMap<Integer, Integer> memory;
 
 	// control signals
 	private int memWrite;
 	private int memRead;
 
 	public Data_Memory() {
-		this.memory = new ArrayList<Integer>();
+		this.memory = new HashMap<Integer, Integer>();
 	}
 
 	// getters & setters
 	public void setAddress(int address) {
 		this.address = address;
 	}
-	
+
 	public void setWrite_Data(int data) {
-		if(this.memWrite == 1)
-			this.memory.set(this.address, data);
+		if (this.memWrite == 1) // this.memory.set(this.address, data);
+			this.memory.put(address, data);
 	}
-	
+
 	public void setMemWrite(int memWrite) {
 		this.memWrite = memWrite;
 	}
-	
+
 	public void setMemRead(int memRead) {
 		this.memRead = memRead;
 	}
-	
+
 	public int getRead_Data() {
-		if(this.memRead == 1)
-			return this.memory.get(this.address);
-		else
-			return -1;
+		if (this.memRead == 1)
+			if (this.memory.get(this.address) != null)
+				return this.memory.get(this.address);
+			
+		return -1;
 	}
 	
-	public void setData(ArrayList<String> data){
-		
+	// input data from the user
+	public void setData(ArrayList<String> data) {
+		for(int i = 0; i < data.size(); i++){
+			String[] s = data.get(i).split("/");
+			this.memory.put(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+		}
 	}
-	
-	public String print(){
-		String s  = "Data Memory Content:\n";
-		for(int i = 0; i < this.memory.size(); i++){
-			s += "	Location #" + i + ": " + this.memory.get(i) + "/" + Integer.toHexString(this.memory.get(i)) + "\n";
+
+	public String print() {
+		String s = "Data Memory Content:\n";
+		for(Map.Entry<Integer, Integer> entry: memory.entrySet()){
+			s += "	Location #" + entry.getKey() + ": " + entry.getValue() + "/"
+					+ Integer.toHexString(entry.getValue()) + "\n";
 		}
 		return s;
 	}
+	
+	/*public static void main(String[] args){
+		HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+		m.put(3, 100);
+		System.out.println("Memory size " + m.size());
+	}*/
 	
 }
